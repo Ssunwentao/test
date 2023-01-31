0903c70b01147a4b6855f83a99fbc525caed51cf
Ssunwentao <837828872@qq.com>
2023-01-31 15:45:46 +0800

    change

---
+++ src/components/Detail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="detail">
+        <img src="" alt="zancun">
+        <p>vnfjgnajngansfdknasdk'flnask'ldfnklsanfdklanfl;dkanf;kanfd;nadfklansv lksnvklankvl'nalkvndklanvklnv'lanvklnas'knvk'lanv'akn'vn,vnfjgnajngansfdknasdk'flnask'ldfnklsanfdklanfl;dkanf;kanfd;nadfklansv lksnvklankvl'nalkvndklanvklnv'lanvklnas'knvk'lanv'akn'vn</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Detail',
+
+}
+</script>
+
+<style scoped>
+#detail {
+    height: auto;
+    width: 100%;
+    /* overflow: hidden; */
+}
+
+img {
+    float: left;
+    width:400px;
+    height:400px;
+    background-color: red;
+}
+
+p {
+    width: 100%;
+    word-wrap: break-word;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="container">
+        <div class="top">
+            <ul class="left">
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+            </ul>
+            <ul class="right">
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+                <li>话费撒方式</li>
+            </ul>
+        </div>
+        <detail></detail>
+    </div>
+  
+</template>
+
+<script>
+import Detail from './Detail.vue';
+export default {
+  components: { Detail },
+    name: 'Home',
+    data (){
+        return ;
+    },
+    component: {
+        Detail
+    }
+
+}
+</script>
+
+<style scoped>
+
+#contanier {
+    margin: 0;
+    padding: 0;    
+}
+.top {
+    height: 80px;
+}
+
+.left {
+    width: 40%;
+    float: left;
+}
+
+.right {
+    width: 40%;
+    float: right;
+}
+
+ul {
+    overflow: hidden;
+}
+
+li {
+    display: inline;
+    margin-right: 20px;
+}
+
+</style>
